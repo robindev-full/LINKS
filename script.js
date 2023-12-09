bab001ed5f99aaf0c41ef71aab68b7c609605be9
robson <robindev7full@gmail.com>
2023-12-09 15:53:50 -0300

    switch

---
+++ script.js
@@ -0,0 +1,10 @@
+function togglemode(){
+    const html = document.documentElement
+    html.classList.toggle('light')
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains('light')){
+        img.setAttribute("src", "assets/nieli2.png")
+    }
+    else{img.setAttribute("src","assets/nieli1.png")}
+}
